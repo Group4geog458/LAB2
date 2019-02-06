fd9d691c1ede9508713c13d12926be09c2a417d3
Macklan <macklanb@uw.edu>
2019-02-05 20:44:07 -0800

    Another attempt

--- OD_data_processing.R
+++ OD_data_processing.R
@@ -1,11 +1,4 @@
 library(tidyverse)
-library(tigris)
-library(tmap)
-library(ggplot2)
-library(leaflet)
-library(stringr)
-library(sf)
-library(tidycensus)
 
 od <- read.csv(file="ok_od_main_JT00_2015.csv")
 
@@ -36,49 +29,18 @@ od <- od %>%
 od_all_one <- od %>% 
   group_by(work_tract, home_tract) %>%
   summarise_all(funs(sum)) %>% 
-  filter(between(work_tract, 40109000000, 40110000000) | between(home_tract, 40109000000, 40110000000))
+  filter(between(work_tract, 40109000000, 40110000000) | between(home_tract, 40109000000, 40110000000)) %>%
+  arrange(desc(S000))
+
+od_test <- od_all_one %>% filter(work_tract == 40109108601)
+
+write.csv(od_test, "OD_TEST_TRACT.csv")
 
 # Both work and residence are within OK county
 od_all_both <- od %>% 
   group_by(work_tract, home_tract) %>%
   summarise_all(funs(sum)) %>% 
   filter(between(work_tract, 40109000000, 40110000000) & between(home_tract, 40109000000, 40110000000))
-
-##########################################################
-
-OD <- od_all_both %>% filter(work_tract == home_tract) %>% select(work_tract, home_tract, S000)
-
-library(tidyverse)
-library(tidycensus)
-library(leaflet)
-library(stringr)
-library(sf)
-library(RColorBrewer)
-
-
-tracts <- tracts("40", "109")
-
-ok_all <- geo_join(tracts, OD, by_sp="GEOID", by_df="work_tract")
-
-
-pal <- colorNumeric("Reds", domain = ok_all$S000)
-
-ok_all <- ok_all %>%
-  leaflet(width = "100%") %>%
-  addProviderTiles(provider = "CartoDB.Positron") %>%
-  addPolygons(popup = ~ str_extract(work_tract, "^([^,]*)"),
-              color = "#444444",
-              weight = 1,
-              smoothFactor = 0,
-              fillOpacity = 0.7,
-              fillColor = ~ pal(S000)) %>%
-  addLegend("bottomright", 
-            pal = pal, 
-            values = ~ S000,
-            title = "Total Number of Jobs",
-            opacity = 1)
-
-plot(ok_all)
 
 
 
